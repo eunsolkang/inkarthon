f7d9bc6abec1b38bf6cd59c933d3eecbdf18aa31
eunsolkang <eunsol2953@gmail.com>
2023-05-13 00:11:49 +0900

    firebase

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter } from 'react-router-dom'
 import { Route, Routes } from 'react-router-dom';
 import Main from './components/Main';
 import NotFound from './components/NotFound';
-
+import {fire} from './lib/firebase'
 
 function App() {
+  fire();
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Main />} ></Route>
+        <Route exact path="/" element={<Main />}></Route>
         <Route component={NotFound}></Route>
       </Routes>
     </BrowserRouter>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,6 @@
 const Login = () => {
     return (
         <div>
-        
         </div>
     )
 }
