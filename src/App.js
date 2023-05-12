19a823e1ed73ac9f2ca9b63affa623407f5e399c
eunsolkang <eunsol2953@gmail.com>
2023-05-13 03:46:44 +0900

    logic

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import NotFound from './components/NotFound';
 import {fire} from './lib/firebase'
 import Vote from './components/Vote';
 import Question from './components/Question';
+import Pending from './components/Pending';
+import Lobby from './components/Lobby';
+import Voted from './components/Voted';
 
 function App() {
   fire();
@@ -16,6 +19,9 @@ function App() {
         <Route exact path="/login" element={<Login />} ></Route>
         <Route exact path="/vote/:team" element={<Vote />} ></Route>
         <Route exact path="/question/:team" element={<Question />} ></Route>
+        <Route exact path="/lobby" element={<Lobby />} ></Route>
+        <Route exact path="/pending" element={<Pending />} ></Route>
+        <Route exact path="/voted" element={<Voted />} ></Route>
         <Route component={NotFound}></Route>
       </Routes>
     </BrowserRouter>
