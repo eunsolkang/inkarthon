33d4b735878ee65e2b7c4772d23dec29b90b8985
eunsolkang <eunsol2953@gmail.com>
2023-05-12 21:33:12 +0900

    style

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,27 @@
+import { styled } from "styled-components";
+
+const StyledMain = styled.div`
+    background-color: blue;
+    height: 100vh;
+    .title{
+        font-size: 4rem;
+        color: white;
+        span{
+            font-size: 3rem;
+        }
+    }
+`
+
 const Main = () => {
     return(
-        <div>
-            메인입니다
-        </div>
+        <StyledMain>
+            <div className="title">
+                2023 Interface Hackarthon
+                <div>
+                    x제주몰빵
+                </div>
+            </div>
+        </StyledMain>
     )
 }
 
